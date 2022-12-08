ffb459b872c0be25c3a2e16057bc16df4383b674
Vanya1000 <Vanya1000@yandex.ru>
2022-12-08 12:42:09 +0300

    feat: implement read line

---
+++ src/components/InitializeReadLine.js
@@ -0,0 +1,24 @@
+import readline from 'readline';
+import actionRouter from '../action/actionRouter.js';
+import parseUserNameArg from './parseUserNameArg.js';
+
+const initializeReadLine = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', (input) => {
+    if (input.trim().toLowerCase() === '.exit') {
+      rl.close();
+    }
+    actionRouter(input);
+  })
+  .on('SIGINT', () => rl.close())
+  .on('close', () => {
+    console.log(`Thank you for using File Manager, ${parseUserNameArg() || unknown}, goodbye!`);
+    process.exit(0);
+  })
+}
+
+export default initializeReadLine;
