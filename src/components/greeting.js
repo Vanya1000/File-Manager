6a39b7464c040d8ae7f03ce6859eaa155513d54f
Vanya1000 <Vanya1000@yandex.ru>
2022-12-08 12:38:00 +0300

    feat: implement greeting

---
+++ src/components/greeting.js
@@ -0,0 +1,17 @@
+import parseUserNameArg from './parseUserNameArg.js';
+
+const noName = 'You dont pass username as argument. Consiqently, you will be greeted by default :('
+const greetingNoName = 'Welcome to the File Manager, unknown!'
+
+const greeting = () => {
+  const name = parseUserNameArg();
+  console.log(name);
+  if (name) {
+    console.log(`Welcome to the File Manager, ${name}!`);
+  } else {
+    console.log(noName);
+    console.log(greetingNoName);
+  }
+};
+
+export default greeting;
