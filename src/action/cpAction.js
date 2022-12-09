1d81dbd0447c12ee5a3c0313b407fb66b19187af
Vanya1000 <Vanya1000@yandex.ru>
2022-12-09 22:51:57 +0300

    fix: check case in copy

--- src/action/cpAction.js
+++ src/action/cpAction.js
@@ -1,20 +1,28 @@
-import { createReadStream, createWriteStream } from 'fs';
-import { join } from 'path';
-import { pipeline } from 'stream/promises';
-import printCurrentDirectory from '../components/currentDirectory.js';
+import { createReadStream, createWriteStream } from "fs";
+import { resolve, parse } from "path";
+import { pipeline } from "stream/promises";
+import printCurrentDirectory from "../components/currentDirectory.js";
+import { isExistFile } from "../utils/utils.js";
 
-const cpAction = async (fileName) => {
-  const [nameSource, nameDestination] = fileName;
-  const currentDir = process.cwd();
-  const filePathSource = join(currentDir, nameSource);
-  const filePathDestination = join(currentDir, nameDestination);
+const cpAction = async (fileNameArgs) => {
   try {
+    if (fileNameArgs.length < 2) throw new Error("Invalid number of arguments");
+    const [nameSource, nameDestination] = fileNameArgs;
+    const currentDir = process.cwd();
+    const filePathSource = resolve(currentDir, nameSource);
+    const { base } = parse(filePathSource);
+    const filePathDestination = resolve(currentDir, nameDestination, base);
+
+    const isExistSourceFile = await isExistFile(filePathSource);
+    if (!isExistSourceFile) throw new Error("File not found");
     const readableStream = createReadStream(filePathSource);
-    const writableStream = createWriteStream(filePathDestination, {flags: 'wx'});
+    const writableStream = createWriteStream(filePathDestination, {
+      flags: "wx",
+    });
     await pipeline(readableStream, writableStream);
     printCurrentDirectory();
   } catch (error) {
-    console.log('Operation failed');
+    console.log("Operation failed");
   }
 };
 
