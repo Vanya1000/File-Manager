676f9231936aa98d288591e7250575d0f782066e
Vanya1000 <Vanya1000@yandex.ru>
2022-12-10 21:56:49 +0300

    feat: implement hash

---
+++ src/action/hashAction.js
@@ -0,0 +1,26 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+import { myWritable } from '../utils/castomWritableStream.js';
+import printCurrentDirectory from '../components/currentDirectory.js';
+const { createHash } = await import('crypto');
+
+const hashAction = async ([fileName]) => {
+  try {
+    if (!fileName) throw new Error('No file name');
+    const pathFile = resolve(fileName);
+    const readableStream = createReadStream(pathFile);
+    const hash = createHash('sha256');
+    const writableStream = new myWritable();
+    await pipeline(
+      readableStream, 
+      hash.setEncoding('hex'), 
+      writableStream
+      );
+    printCurrentDirectory();
+  } catch (error) {
+    console.log('Operation failed');
+  }
+};
+
+export default hashAction;
